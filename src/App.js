7303d95b875ece184a759463640d8e375b1a3f01
web-altaf-1 <web.altaf.1@gmail.com>
2022-06-25 21:58:44 +0600

    first commit and initial set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {ChatEngine} from 'react-chat-engine';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+      height='100vh'
+      projectId=''
+      userName=''
+      userSecret=''
+    >
+
+    </ChatEngine>
   );
 }
 
